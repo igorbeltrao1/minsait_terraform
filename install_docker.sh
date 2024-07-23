c7b8d82cde83fc280d1baee617a819b1d347e6c5
igorbeltrao <igorbeltrao96@gmail.com>
2024-07-23 16:30:03 -0300

    modificacao do install_docker.sh
    Former-commit-id: 412ba1cec40a5a84b2270def1710c074ae5dd7ec

--- install_docker.sh
+++ install_docker.sh
@@ -1,34 +1,42 @@
-# Remover versões antigas do Docker
-sudo apt-get -y remove docker docker-engine docker.io
+#!/bin/bash
+##@@@@
+## Scrip cai dat docker, docker-compose tren CentOS 7
+## Cach thuc hien 
+### yum install wget -y
+### wget https://raw.githubusercontent.com/nhanhoadocs/scripts/master/Utilities/install_docker.sh
+### chmod +x install_docker.sh
+### bash install_docker.sh
+##@@@@
 
-# Atualizar pacotes e instalar dependências
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates wget software-properties-common
+echo "Cai dat cac pham mem tien ich"
+sleep 3
+yum update -y
+yum install -y yum-utils device-mapper-persistent-data lvm2 wget
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Adicionar chave GPG do Docker
-wget https://download.docker.com/linux/debian/gpg
-sudo apt-key add gpg
+echo "Cai dat container"
+sleep 3
+yum install -y docker-ce docker-ce-cli containerd.io
 
-# Adicionar repositório Docker
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
+echo "Khoi dong docker"
+sleep 3
 
-# Atualizar novamente e instalar Docker
-sudo apt-get update
-sudo apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
+systemctl start docker 
+systemctl enable docker 
 
-# Verificar status do Docker
-sudo systemctl status docker
+echo "Phien ban docker da cai dat"
+docker --version
 
-# Adicionar o usuário atual ao grupo docker
-sudo usermod -aG docker $(whoami)
-
-# Docker Compose
-sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.17.1/docker-compose-$(uname -s)-$(uname -m)"
+echo "Cai dat docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-# Verificar a versão do Docker Compose
+echo "Kiem tra phien ban docker-compose"
+sleep 3
 docker-compose -v
+
+echo "I.A.OK"
 
 # Subir o container do MySQL
 sudo docker run --name mysql \
